6c17fea02ddca7db074a19322b41a5cbc40322c4
Saubhagya Srivastava <Saubhagyasrivastav4@gmail.com>
2025-02-22 16:55:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pickle
 import numpy as np
 
 # Load the trained model
-with open("model.pkl", "rb") as file:
+with open("new_model.pkl", "rb") as file:
     model = pickle.load(file)
 
 # Title of the app
